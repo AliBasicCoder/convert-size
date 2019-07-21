655701fa9ed97f9b3613311c501c22d4c8e4a019
AliBasicCoder <aliahmedreda34@gmail.com>
2019-07-21 21:18:04 +0200

    ..

--- README.md
+++ README.md
@@ -1,20 +1,27 @@
 # convert-size
+
 [![npm version](https://img.shields.io/static/v1.svg?label=npm%20version&message=1.0.0&color=green)](https://www.npmjs.com/package/convert-size)
 [![LICENSE](https://img.shields.io/static/v1.svg?label=LICENSE&message=MIT&color=green)](https://github.com/AliBasicCoder/convertSize/blob/master/LICENSE)
 
 convert-size is a package to convert a size with bytes to 
-units sush as KB MB TB
+units such as KB MB TB
 
-## exmaples
+## Usage
+
 ```js
 const { convertSize } = require('convert-size');
 // or
 import { convertSize } from 'convert-size';
 
-console.log(convertSize(1024)); // output 1 KB
-console.log(convertSize(1024 * 1024)); // output 1 MB
-console.log(convertSize(1024 * 1024 * 1024)); // output 1 GB
-console.log(convertSize(1024 * 1024 * 1024 * 1024)); // output 1 TB
-console.log(convertSize(1024 * 1024 * 1024 * 1024 * 1024)); // output 1 PB
+convertSize(1024); // => 1.00 KB
+convertSize(1024 * 1024); // => 1.00 MB
+
+convertSize (
+    1024 * 1024,
+    {
+        accuracy: 0,
+        shortcut: false
+    }
+) // => 1 Kilo Byte
 
 ```
--- index.ts
+++ index.ts
@@ -1,27 +1,55 @@
-export const convertSize = (size: number): string => {
-    if (size <= 1) {
-        return (size + " byte");
-    }
-    if (size < 1024) {
-        return (size + " bytes");
-    }
-    const kb = size / 1024;
-    const mb = kb / 1024;
-    const gb = mb / 1024;
-    const tb = gb / 1024;
-    const bb = tb / 1024;
-    if (kb < 1024) {
-        return (kb.toPrecision(3) + " KB");
-    }
-    if (mb < 1024) {
-        return (mb.toPrecision(3) + " MB");
-    }
-    if (gb < 1024) {
-        return (gb.toPrecision(3) + " GB");
-    }
-    if (tb < 1024) {
-        return (tb.toPrecision(3) + " TB");
-    }
-    return (bb.toPrecision(3) + " PB");
-};
 
+interface Options {
+    lowerCase: boolean;
+    accuracy: number;
+    shortcut: boolean;
+}
+
+const options = {
+    lowerCase: false,
+    accuracy: 2,
+    shortcut: true
+}
+
+export const convertSize = (size: number, op: Options = options): string => {
+
+    const func = (thing: string): string => op.lowerCase ? thing.toLowerCase() : thing;
+
+    const
+        kb = size / 1024,
+        mb = kb / 1024,
+        gb = mb / 1024,
+        tb = gb / 1024,
+        pb = tb / 1024,
+        byte = op.shortcut ? func('B') : func('Byte'),
+        kiloByte = op.shortcut ? func("KB") : func('Kilo Byte'),
+        megaByte = op.shortcut ? func("MB") : func('Mega Byte'),
+        gigaByte = op.shortcut ? func("GB") : func('Giga Byte'),
+        teraByte = op.shortcut ? func("TB") : func('Tera Byte'),
+        petaByte = op.shortcut ? func("PB") : func('Peta Byte'),
+        per = op.accuracy || 3;
+    // END
+
+
+    if (size < 1024) {
+        return `${size} ${byte}${size > 1 && !op.shortcut ? 's' : ''}`;
+    }
+
+    if (kb < 1024) {
+        return `${kb.toFixed(per)} ${kiloByte}${kb > 1 && !op.shortcut ? 's' : ''}`;
+    }
+
+    if (mb < 1024) {
+        return `${mb.toFixed(per)} ${megaByte}${mb > 1 && !op.shortcut ? 's' : ''}`;
+    }
+
+    if (gb < 1024) {
+        return `${gb.toFixed(per)} ${gigaByte}${gb > 1 && !op.shortcut ? 's' : ''}`;
+    }
+
+    if (tb < 1024) {
+        return `${tb.toFixed(per)} ${teraByte}${tb > 1 && !op.shortcut ? 's' : ''}`;
+    }
+
+    return `${pb.toFixed(per)} ${petaByte}${pb > 1 ? 's' : ''}`;
+}
